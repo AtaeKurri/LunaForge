f4d0cab269cceb2348d2ecd82384b1ac707e3e81
Tania <hakussura@protonmail.com>
2024-08-27 18:15:09 +0200

    Ajoutez des fichiers projet.

---
+++ Aviator_Omega/Program.cs
@@ -0,0 +1,7 @@
+﻿/*
+using Aviator_Omega.Windows;
+
+MainWindow mainWindow = new();
+Thread renderThread = new(mainWindow.Start().Wait);
+renderThread.Start();
+*/
---
+++ Aviator_Omega/Windows/MainWindow.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Aviator_Omega.Windows;
+
+public class MainWindow
+{
+    
+}
